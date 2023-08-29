ab41fe598ce69eb1c7ec8f5f5cff07f7ab6e2967
quinn-getty <quinnn.gao@gmail.com>
2023-08-30 01:28:08 +0800

    fix: 引入 cobra

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,12 +3,42 @@ package cmd
 import (
 	"mangosteen/internal/database"
 	"mangosteen/internal/router"
+
+	"github.com/spf13/cobra"
 )
 
-func RunServer() {
+func Run() {
+	rootCmd := &cobra.Command{
+		Use: "mangosteen",
+	}
+
+	serverCmd := &cobra.Command{
+		Use: "server",
+		Run: func(cmd *cobra.Command, args []string) {
+			RunServer()
+		},
+	}
+
+	dbCmd := &cobra.Command{
+		Use: "db",
+	}
+
+	dbCreateCmd := &cobra.Command{
+		Use: "create",
+		Run: func(cmd *cobra.Command, args []string) {
+			database.CreateTables()
+		},
+	}
+
+	rootCmd.AddCommand(serverCmd)
+	rootCmd.AddCommand(dbCmd)
+	dbCmd.AddCommand(dbCreateCmd)
 	database.Connect()
-	database.CreateTables()
 	defer database.Close()
+	rootCmd.Execute()
+}
+
+func RunServer() {
 	r := router.New()
 	r.Run(":8080")
 }
