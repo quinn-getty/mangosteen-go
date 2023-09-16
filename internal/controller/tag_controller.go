df4d839c4f1c4a95cdbc3c39430d03082e67ec32
quinn-getty <quinnn.gao@gmail.com>
2023-09-16 14:42:45 +0000

    feat: 创建tag controller

--- internal/controller/tag_controller.go
+++ internal/controller/tag_controller.go
@@ -6,6 +6,9 @@ type TagController struct {
 }
 
 func (ctrl *TagController) RegisterRouter(rg *gin.RouterGroup) {
+	item := rg.Group("/tag")
+	item.POST("", ctrl.Create)
+	item.GET("", ctrl.getList)
 
 }
 func (ctrl *TagController) Create(c *gin.Context)  {}
